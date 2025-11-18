f7ae7fb302a94dfbab5de6b6ce767109328ea3af
Health-e-Sphere <healthesphere@gmail.com>
2025-11-18 07:29:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-109280062-71202",
+  "appId": "1:499260146109:web:f92397f06d07e5e57b92d4",
+  "apiKey": "AIzaSyD-5hJro1ZVWU6Q9OKSteGsm5seeYXhOcM",
+  "authDomain": "studio-109280062-71202.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "499260146109"
+};
