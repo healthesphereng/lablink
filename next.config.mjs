750d1efb9fe57a8ca9d7a94ca121bbf9b4fbbe7c
healthesphereng <healthesphere@gmail.com>
2026-02-10 18:55:36 +0100

    fix: downgrade next.js to 15.1.6 and config PWA correctly

---
+++ next.config.mjs
@@ -0,0 +1,40 @@
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default withPWA(nextConfig);
